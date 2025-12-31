427da8dca9b0c8757ba0675f39942e30202392e3
alihassanafzal224-cell <alihassanafzal224@gmail.com>
2025-12-31 14:20:19 +0500

    remove extra

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import connectDatabase from "./config/database.js";
 import app from "./app.js";
-import cloudinary  from "./config/cloudinary.js";
 
 dotenv.config();
 
@@ -12,30 +11,7 @@ const startServer = async () => {
 
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server running on port ${process.env.PORT || 8000}`);
-        });
-
-        // Cloudinary upload example
-        const uploadResult = await cloudinary.uploader
-            .upload(
-                "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
-                { public_id: "shoes" }
-            )
-            .catch((error) => console.log("Upload error:", error));
-
-        // Optimized delivery URL
-        const optimizeUrl = cloudinary.url("shoes", {
-            fetch_format: "auto",
-            quality: "auto",
-        });
-
-        // Auto-crop and transform image
-        const autoCropUrl = cloudinary.url("shoes", {
-            crop: "fill",
-            gravity: "auto",
-            width: 500,
-            height: 500,
-        });
-        
+        })
 
     } catch (error) {
         console.error("Server error:", error);
